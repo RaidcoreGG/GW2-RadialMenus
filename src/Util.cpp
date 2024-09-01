4251a30c1bbb900576c58ee66f3fa89f2b7d3488
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-09-01 23:29:07 +0200

    unset context code

--- src/Util.cpp
+++ src/Util.cpp
@@ -136,7 +136,7 @@ namespace Input
 		lp.ScanCode = MapVirtualKeyA(aVKey, MAPVK_VK_TO_VSC);
 		lp.ExtendedFlag = lp.ScanCode & 0xE000 ? 1 : 0;
 		lp.Reserved = 0;
-		lp.ContextCode = aIsSystem ? 1 : 0;
+		lp.ContextCode = 0;// aIsSystem ? 1 : 0;
 		lp.PreviousKeyState = aIsDown ? 0 : 1;
 		lp.TransitionState = aIsDown ? 0 : 1;
 
